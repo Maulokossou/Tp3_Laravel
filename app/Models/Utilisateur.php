3a39ca53f84dc947e8533ecf266d3d77beed92ef
Maurel <maestrolokossou2001@gmail.com>
2023-08-27 23:35:31 +0200

    Fichiers modifiés

---
+++ app/Models/Utilisateur.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Utilisateur extends Model
+{
+    protected $fillable = ["firstname","lastname","birthday","password","email","emailVerified","verify_at"];
+    protected $table ="user";
+}
